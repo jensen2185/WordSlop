bdafe89b9a0b9e61c90f6c7ab78e3c8c42464fec
jensen <jensen2185@gmail.com>
2025-08-05 12:58:23 -0700

    Update version code to 6 for Play Store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wordslop.game"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 6
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
